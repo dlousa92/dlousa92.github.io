b9651ab9584bfa6634fe9c6a870871baa1565fe3
Daniel Lousa <dlousa92@gmail.com>
2018-07-24 14:30:56 -0400

    make my projects collapsible

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// code for collapsible my projects tab
+
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.collapsible')
   var instances = M.Collapsible.init(elems)
