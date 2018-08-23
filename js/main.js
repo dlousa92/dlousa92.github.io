8a14282aa7293b7f305cf80e92f5c3f508de5058
Daniel Lousa <dlousa92@gmail.com>
2018-08-23 15:53:16 -0400

    create array where color schemes will be stored

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.collapsible')
   var instances = M.Collapsible.init(elems)
 })
+
+// code for color scheme switch button
+
+const colorSchemes = [
+  {
+    primarycolor: '#F98866',
+    secondarycolor: '#80BD9E'
+  }, 
+  {}, {}, {}]
