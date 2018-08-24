1847c700f48697c41c3d86f1410235ee7a4c76c4
Daniel Lousa <dlousa92@gmail.com>
2018-08-24 13:14:04 -0400

    make it so its not possible to get the same color scheme back to back

--- js/main.js
+++ js/main.js
@@ -32,14 +32,26 @@ const colorSchemes = [
   }
 ]
 
+let currentScheme
+
 function changeColor (e) {
-  e.preventDefault()
+  if (e) {
+    e.preventDefault()
+  }
 
-  let index = Math.floor(Math.random() * Math.floor(6))
-  let newScheme = colorSchemes[index]
+  let index = Math.floor(Math.random() * Math.floor(6)) // generate a number between 0 and 5
+  let newScheme = colorSchemes[index] // save number as an index in the colorSchemes array.
 
-  const html = document.getElementsByTagName('html')[0]
-  html.style.cssText = `--primary-color: ${newScheme.primarycolor}; --secondary-color: ${newScheme.secondarycolor};`
+  if (index !== currentScheme) { // if number just generated does not equal the number generated last time, then proceed and choose the new color scheme
+    const html = document.getElementsByTagName('html')[0]
+    html.style.cssText = `--primary-color: ${newScheme.primarycolor}; --secondary-color: ${newScheme.secondarycolor};`
+
+    currentScheme = index
+    console.log('color scheme chosen')
+  } else {
+    changeColor()
+    console.log('running again')
+  }
 }
 
 const button = document.querySelector('.change-color')
